72afa8f9a300fdfd26a41a5a4b7137719165c3be
Matthew Kaney <matthew.s.kaney@gmail.com>
2025-02-14 14:32:45 -0500

    Fix build base path

--- src/playground/vite.config.ts
+++ src/playground/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/punchcard/",
   // build: {
   //   rollupOptions: {
   //     input: {
